f32af73c4424f13b2942f1a1005f76d1f690d6a5
Jeremy Leung <jeremyleung521@gmail.com>
2021-02-02 16:03:54 -0500

    fixed deprecated sphinx option and removed wiki_images

--- doc/conf.py
+++ doc/conf.py
@@ -297,4 +297,4 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 def setup(app):
-    app.add_stylesheet('custom.css')
+    app.add_css_file('custom.css')
