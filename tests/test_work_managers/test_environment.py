0eeba8765469b68a9ada82a51683459cc5a68cda
Jeremy Leung <jml230@pitt.edu>
2022-10-05 13:25:41 -0400

    continue to tune the timeout factor

--- tests/test_work_managers/test_environment.py
+++ tests/test_work_managers/test_environment.py
@@ -79,9 +79,7 @@ class TestInstantiations(unittest.TestCase):
     def testZeroMQ(self):
         os.environ['WM_WORK_MANAGER'] = 'zmq'
         os.environ['WM_N_WORKERS'] = str(3)
-        os.environ['WM_ZMQ_TIMEOUT_FACTOR'] = str(10.0)
-        os.environ['WM_ZMQ_MASTER_HEARTBEAT'] = str(40)
-        os.environ['WM_ZMQ_WORKER_HEARTBEAT'] = str(40)
+        os.environ['WM_ZMQ_TIMEOUT_FACTOR'] = str(20.0)
         work_manager = make_work_manager()
         assert isinstance(work_manager, ZMQWorkManager)
         with work_manager:
