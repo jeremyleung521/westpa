Bug fixes for the units conversion thing.

src/westpa/core/trajectory.py
@@ -373,12 +373,14 @@ def load_netcdf(folder):
     datasets = {'coordinates': coords, 'cell_lengths': cell_lengths, 'cell_angles': cell_angles, 'time': time}
     convert = ['coordinates', 'cell_lengths']  # Length-based datasets that need to be converted
 
-    for key, val in datasets:
+    for key in datasets.keys():
         if key in rootgrp.variables:
             if key in convert:
-                val = in_units_of(np.asarray(rootgrp.variables[key][:]), units_in='nanometers', units_out='angstrom', inplace=True)
+                datasets[key] = in_units_of(
+                    np.asarray(rootgrp.variables[key][:]), units_in='angstrom', units_out='nanometers', inplace=True
+                )
             else:
-                val = np.asarray(rootgrp.variables[key][:])  # noqa: F841
+                datasets[key] = np.asarray(rootgrp.variables[key][:])  # noqa: F841
 
     traj = WESTTrajectory(coordinates=coords, unitcell_lengths=cell_lengths, unitcell_angles=cell_angles, time=time)
 