ee931ee583a86363c863e1991d8f2bed83f9537d
Jeremy M. G. Leung <jml230@pitt.edu>
2024-11-04 15:43:32 -0500

    clean up

--- src/westpa/core/yamlcfg.py
+++ src/westpa/core/yamlcfg.py
@@ -347,13 +347,7 @@ class YAMLSystem:
         self.bin_mapper = NopMapper()
         # self.bin_mapper = None
         self._bin_target_counts = None
-
         self.bin_target_counts = [1]
-
-        # For calculating sample density
-        self.ideal_sample_density = None
-        self.sample_density = 1
-        self.max_target_count_multiplier = 1
 
     @property
     def bin_target_counts(self):
