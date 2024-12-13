bd191b25906da280719e3235c9cb193728a8a549
Jeremy Leung <jml230@pitt.edu>
2024-12-13 13:33:32 -0500

    sample density, ignore bin mappers without sample density attribute

--- src/westpa/core/data_manager.py
+++ src/westpa/core/data_manager.py
@@ -955,7 +955,10 @@ class WESTDataManager:
 
         with self.lock:
             iter_group = self.get_iter_group(n_iter)
-            iter_group.attrs['sample_density'] = self.system.sample_density or 1
+            try:
+                iter_group.attrs['sample_density'] = self.system.sample_density or 1
+            except AttributeError:
+                iter_group.attrs['sample_density'] = 1
 
             pc_dsid = iter_group['pcoord'].id
             si_dsid = iter_group['seg_index'].id
